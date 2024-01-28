2413f3f0d87eac8b08afa4bddebb31059c959f0e
Syed023 <152625426+Syed023@users.noreply.github.com>
2024-01-28 20:49:16 +0530

    Add files via upload

--- Hill cipher.c
+++ Hill cipher.c
@@ -1,65 +1,44 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main(){
+unsigned int a[3][3]={{6,24,1},{13,16,10},{20,17,15}};
+unsigned int b[3][3]={{8,5,10},{21,8,21},{21,12,8}};
+int i,j, t=0;
+unsigned int c[20],d[20];
+char msg[20];
 
-int isUpper(char *s)
-{
-    int i, len = strlen(s);
-    for (i = 0; i < len; i++)
-    {
-    if (s[i] < 'A' || s[i] > 'Z')
-    return 0;
-    }
-    return 1;
+printf("Enter plain text\n ");
+scanf("%s",msg);
+for(i=0;i<strlen(msg);i++)
+{ c[i]=msg[i]-65;
+printf("%d ",c[i]);
 }
-void matrixMultiply(int a[3][3], int b[3][1], int c[3][1], int n)
+for(i=0;i<3;i++)
+{ t=0;
+for(j=0;j<3;j++)
 {
-    int i, j, k, sum;
-    for (i = 0; i < n; i++)
-    {
-    sum = 0;
-    for (j = 0; j < n; j++)
-    {
-    sum += a[i][j] * b[j][0];
-    }
-    c[i][0] = sum % 26;
-    }
+t=t+(a[i][j]*c[j]);
 }
-void encrypt(char *plainText, int key[3][3], char *cipherText)
+d[i]=t%26;
+}
+printf("\nEncrypted Cipher Text :");
+for(i=0;i<3;i++)
+printf(" %c",d[i]+65);
+for(i=0;i<3;i++)
 {
-    int i, j, k, len;
-    int plainVector[3][1], cipherVector[3][1];
-    len = strlen(plainText);
-    for (i = 0; i < len; i += 3)
-    {
-    for (j = 0; j < 3; j++)
-    {
-    if (i + j < len)
-    plainVector[j][0] = plainText[i + j] - 'A';
-    else
-    plainVector[j][0] = 23;
-    }
-    matrixMultiply(key, plainVector, cipherVector, 3);
-    {
-    cipherText[i + k] = cipherVector[k][0] + 'A';
-    }
-    }
-
-    cipherText[i] = '\0';
+t=0;
+for(j=0;j<3;j++)
+{
+t=t+(b[i][j]*d[j]);
+}
+c[i]=t%26;
+}
+printf("\nDecrypted Cipher Text :");
+for(i=0;i<3;i++)
+printf(" %c",c[i]+65);
+getch();
+return 0;
 }
 
-int main()
-{
-    char plainText[100], cipherText[100];
-    int key[3][3] = {{17, 17, 5}, {21, 18, 21}, {2, 2, 19}};
-    printf("Enter the plain text: ");
-    scanf("%s", plainText);
-    if (!isUpper(plainText))
-    {
-        printf("Invalid input. Only uppercase letters are allowed.\n");
-        return -1;
-    }
-    encrypt(plainText, key, cipherText);
-    printf("The cipher text is: %s\n", cipherText);
-    return 0;
-}
+
